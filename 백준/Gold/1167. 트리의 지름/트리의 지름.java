6fb58da5cf0154e417770591a3f01eca6163d430
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2025-01-11 16:36:43 +0900

    [Gold II] Title: 트리의 지름, Time: 684 ms, Memory: 93196 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,76 +1,67 @@
-import java.io.*;
-import java.util.*;
-
-import static java.util.Collections.sort;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Node>[] graph;
-    static boolean[] visited;
+    static ArrayList<int[]>[] tree;
+    static int[] visited;
+    static int[] max = {0, 0};
     static Queue<Integer> queue;
-    static int[] distance;
-    static int Max;
     public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
-       int v = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[v+1];
-        for(int i = 1; i <= v; i++){
-            graph[i] = new ArrayList<Node>();
-        }
-        for(int i = 0; i < v; i++){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        tree = new ArrayList[v+1];
+        visited = new int[v+1];
+        for (int i=1; i <= v; i++){
+            visited[i] = -1;
             st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            tree[start] = new ArrayList();
             while(true){
-                int e = Integer.parseInt(st.nextToken());
-                if(e == -1){
-                    break;
-                }
-                int w = Integer.parseInt(st.nextToken());
-                graph[s].add(new Node(e,w));
-                graph[e].add(new Node(s,w));
+                int num = Integer.parseInt(st.nextToken());
+                if(num == -1) break;
+                int num2 = Integer.parseInt(st.nextToken());
+                tree[start].add(new int[] {num, num2});
             }
         }
-        distance = new int[v+1];
-        visited = new boolean[v+1];
-        BFS(1);
-        Max = 0;
-        int index = 1;
-        for(int i=2; i <=v; i++){
-            if(Max<distance[i]){
-                Max = distance[i];
-                index = i;
-            }
-        }
-        distance = new int[v+1];
-        visited = new boolean[v+1];
-
-        BFS(index);
-        Arrays.sort(distance);
-        System.out.println(distance[v]);
+        queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = 0;
+        int node = BFS();
+        queue = new LinkedList<>();
+        visited = new int[v+1];
+        queue.add(node);
+        for (int i=1; i <= v; i++) visited[i] = -1;
+        visited[node] = 0;
+        BFS();
+        System.out.println(max[1]);
     }
-    static void BFS(int n){
-        visited[n]= true;
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(n);
+    static int BFS(){
         while(!queue.isEmpty()){
-            int popped = queue.poll();
-            for(Node node : graph[popped]){
-                if (!visited[node.num]){
-                    visited[node.num] = true;
-                    queue.offer(node.num);
-                    distance[node.num] = node.weight + distance[popped];
+            int node = queue.poll();
+            for (int[] i : tree[node]) {
+                int newNode = i[0];
+                int distance = i[1];
+                if(visited[newNode] ==-1){
+                    visited[newNode] = visited[node] + distance;
+                    queue.add(newNode);
+                    if(max[1] < visited[newNode]){
+                        max[0] = newNode;
+                        max[1] = visited[newNode];
+                    }
                 }
+
             }
         }
+        return max[0];
 
-    }
-    static class Node{
-        int num;
-        int weight;
-        public Node(int num, int weight){
-            this.num = num;
-            this.weight = weight;
-        }
     }
 
 }
+
+
