6da448d287eaf28cfa3479daf5882b9f10eb9124
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2025-03-22 21:10:43 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 772 ms, Memory: 103936 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        int m = sc.nextInt();
+        long[] array = new long[n];
+        long num = 0;
+
+        for(int i = 0; i < n; i++){
+            array[i] = sc.nextLong();
+        }
+
+        for(int i = 0; i < m; i++){
+            num += array[i];
+        }
+
+        long maxSum = num;
+
+        for(int i = m; i < n; i++){
+            num += array[i] - array[i - m];
+            maxSum = Math.max(maxSum, num);
+        }
+
+        System.out.println(maxSum);
+    }
+}
