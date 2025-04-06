2f2b2f482da62da32ce2236a0054f4e29a0745f7
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2025-04-06 13:15:28 +0900

    [Gold V] Title: 토마토, Time: 556 ms, Memory: 128044 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,20 +3,18 @@ import java.io.*;
 
 public class Main {
     static int[][] graph;
-    static int[] move_r = {1, 0, -1, 0};
-    static int[] move_c = {0, 1, 0, -1};
+    static int[] move_r = new int[]{1,0,-1,0};
+    static int[] move_c = new int[]{0,1,0,-1};
     static int n, m;
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken()); // 열
-        n = Integer.parseInt(st.nextToken()); // 행
-        graph = new int[n + 1][m + 1];
-
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        graph = new int[n+1][m+1];
         Queue<int[]> q = new LinkedList<>();
         for(int i = 1; i <= n; i++){
-            st = new StringTokenizer(br.readLine());
+            st =  new StringTokenizer(br.readLine());
             for(int j = 1; j <= m; j++){
                 graph[i][j] = Integer.parseInt(st.nextToken());
                 if(graph[i][j] == 1){
@@ -24,22 +22,19 @@ public class Main {
                 }
             } 
         }
-
         int date = 0;
         while(!q.isEmpty()){
             int[] node = q.poll();
-            int r = node[0], c = node[1], d = node[2];
-            date = Math.max(date, d);
-            for(int i = 0; i < 4; i++){
-                int newr = r + move_r[i];
-                int newc = c + move_c[i];
+            date = Math.max(date, node[2]);
+            for(int i = 0; i< 4; i++){
+                int newr = node[0] + move_r[i];
+                int newc = node[1] + move_c[i];
                 if(ingraph(newr, newc)){
-                    graph[newr][newc] = 1; // 방문 체크를 여기서!
-                    q.offer(new int[]{newr, newc, d + 1});
+                    q.offer(new int[]{newr, newc, node[2] + 1});
+                    graph[newr][newc] = 1;
                 }
             }
         }
-
         boolean flag = false;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
@@ -50,11 +45,16 @@ public class Main {
             }
             if(flag) break;
         }
-
-        System.out.println(flag ? -1 : date);
+        if(flag) System.out.println(-1); 
+        else System.out.println(date);
+       
     }
-
     static boolean ingraph(int r, int c){
-        return r > 0 && r <= n && c > 0 && c <= m && graph[r][c] == 0;
+        if(r > 0 && r <= n && c > 0 && c <= m && graph[r][c] == 0) return true;
+        return false;
     }
+
 }
+/*
+    정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토
+*/
