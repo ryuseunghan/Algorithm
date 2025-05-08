824d1764f450eeaa1118174c412a8b5aa78f57ca
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2025-05-08 12:04:18 +0900

    [level 3] Title: 불량 사용자, Time: 111.51 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,35 +1,24 @@
-import java.util.*;
 class Solution {
-    static List<String>[] list;
-    static Set<Set<String>> set = new HashSet<>();
-    static int banlen;
+    static int[] arr;
     public int solution(String[] user_id, String[] banned_id) {
-        banlen = banned_id.length;
-        list = new ArrayList[banlen];
-        for(int i = 0 ; i < banlen; i++){
-            list[i] = new ArrayList<>();
+        arr = new int[banned_id.length];
+        int idx = 0;
+        for(String ban : banned_id){
+            int num = 0;
             for(String user : user_id){
-                if(check(banned_id[i], user)){
-                    list[i].add(user);
+                if(check(ban, user)){
+                    arr[idx] += Math.pow(2, num);
                 }
+                num++;
             }
+            idx++;
         }
-        dfs(0, new HashSet<>());
-        return set.size();
-    }
-    static void dfs(int idx, Set<String> subset) {
-        if (idx == banlen) {
-            set.add(new HashSet<>(subset));
-            return;
+        for(int i = 0; i < banned_id.length;i++){
+            System.out.println(arr[i]);
         }
-
-        for (String name : list[idx]) {
-            if (!subset.contains(name)) {
-                Set<String> newSet = new HashSet<>(subset);
-                newSet.add(name);
-                dfs(idx + 1, newSet);
-            }
-        }
+        
+        int answer = 0;
+        return answer;
     }
     static boolean check(String ban, String user){
         // 길이 다를 시 false
