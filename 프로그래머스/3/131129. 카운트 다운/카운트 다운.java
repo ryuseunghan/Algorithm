425abdf69d7b891ade9689f0ed0e01b75cfe2277
Seunghan Ryu <106146847+ryuseunghan@users.noreply.github.com>
2025-05-10 20:13:09 +0900

    [level 3] Title: 카운트 다운, Time: 40.87 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.java
@@ -11,45 +11,38 @@ class Solution {
         dp[0][0] = 0;
         for(int i = 1; i <= target; i++){
             for(int j = 1; j<=20; j++){
-                int b_or_s = -1;
-                // 불을 쏘는 경우
-                if(i-50>=0){
-                    if(dp[i][0] > dp[i-50][0]+1){ // 더 적게 던지는 경우로 갱신
-                        dp[i][0] = dp[i-50][0]+1;
-                        dp[i][1] = dp[i-50][1]+1;
-                    }
-                    else if(dp[i][0] == dp[i-50][0]+1){
-                        dp[i][1] = Math.max(dp[i-50][1]+1, dp[i][1]);
+                //불을 쏘는 경우
+                if(i >= 50){
+                    if(dp[i][0] > dp[i-50][0] + 1){
+                        dp[i][0] = dp[i-50][0] + 1;
+                        dp[i][1] = dp[i-50][1] + 1;
+                    }else if(dp[i][0] == dp[i-50][0] + 1 && dp[i][1] < dp[i-50][1] + 1){
+                        dp[i][1] = dp[i-50][1] + 1;
                     }
                 }
-                
                 // 싱글을 쏘는 경우
-                if(i-j>=0){
-                    if(dp[i][0] > dp[i-j][0]+1){
-                        dp[i][0] = dp[i-j][0]+1;
-                        dp[i][1] = dp[i-j][1]+1;
-                    }
-                    else if(dp[i][0] == dp[i-j][0]+1){
-                        dp[i][1] = Math.max(dp[i-j][1]+1, dp[i][1]);
+                if(i >= j){
+                    if(dp[i][0] > dp[i-j][0] + 1){
+                        dp[i][0] = dp[i-j][0] + 1;
+                        dp[i][1] = dp[i-j][1] + 1;
+                    }else if(dp[i][0] == dp[i-j][0] + 1 && dp[i][1] < dp[i-j][1] + 1){
+                        dp[i][1] = dp[i-j][1] + 1;
                     }
                 }
-                
                 // 더블을 쏘는 경우
-                if(i-2*j>=0){
-                    if(dp[i][0] > dp[i-2*j][0]+1){
-                        dp[i][0] = dp[i-2*j][0]+1;
-                        dp[i][1] = dp[i-2*j][1];
+                if(i >= j*2){
+                    if(dp[i][0] > dp[i-j*2][0] + 1){
+                        dp[i][0] = dp[i-j*2][0] + 1;
+                        dp[i][1] = dp[i-j*2][1];
                     }
                 }
-                
                 // 트리플을 쏘는 경우
-                if(i-3*j>=0){
-                    if(dp[i][0] > dp[i-3*j][0]+1){
-                        dp[i][0] = dp[i-3*j][0]+1;
-                        dp[i][1] = dp[i-3*j][1];
+                if(i >= j*3){
+                    if(dp[i][0] > dp[i-j*3][0] + 1){
+                        dp[i][0] = dp[i-j*3][0] + 1;
+                        dp[i][1] = dp[i-j*3][1];
                     }
                 }
-
             }
         }
         return new int[]{dp[target][0], dp[target][1]};
